25bd50eea47013f18897b30456a5a8d024b2f9aa
happysnaker <1637318597@qq.com>
2021-12-28 12:36:52 +0800

    修复 main.cpp

--- AL五子棋/源.cpp
+++ AL五子棋/源.cpp
@@ -10,10 +10,10 @@
 using namespace std;
 constexpr int  Width = 800;
 constexpr int  High = 640;
-vector<vector<int>> map(18, vector<int>(18, -1)), cop(map);		/*map是棋盘，-1空，0电脑，1友方*/
-stack<pair<int, int>> stk;			/*用以控制悔棋*/
-double myPos = 0, aiPos = 0;		/*我方和电脑棋局得分*/
-int aiset;							/*记录电脑上一步下的位置*/
+vector<vector<int>> map(18, vector<int>(18, -1)), cop(map);		//map是棋盘，-1空，0电脑，1友方
+stack<pair<int, int>> stk;			//用以控制悔棋
+double myPos = 0, aiPos = 0;		//我方和电脑棋局得分
+int aiset;							//记录电脑上一步下的位置
 
 int main() {
 	MyChess mychs;
@@ -25,8 +25,8 @@ int main() {
 Start:
 	map = cop;
 	mychs.steps = 0;
-	initgraph(Width,High);
-	while (1) {		
+	initgraph(Width, High);
+	while (1) {
 		ai.Get_key_Setchess(mychs);	//Al first set chess
 		s.show(mychs, user);		//show the map
 		if (ai.gameOver(s, user))	goto Start;
